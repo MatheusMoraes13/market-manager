02064c3fca522ddd429508c5ffb4011ff77bc0cc
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-20 04:34:35 -0300

    Create 20/05 crate market manager repo

---
+++ src/main/java/me/matheusmoraes/market_manager/MarketManagerApplication.java
@@ -0,0 +1,13 @@
+package me.matheusmoraes.market_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/matheusmoraes/market_manager/MarketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.matheusmoraes.market_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
